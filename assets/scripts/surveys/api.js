7138003ae74d2384e9a055df648e5a0921a94612
Andrew.Easterling <Anddrew914@gmail.com>
2017-05-24 11:36:29 -0400

    Streamlines UX,

--- assets/scripts/surveys/api.js
+++ assets/scripts/surveys/api.js
@@ -5,7 +5,7 @@ const store = require('../store.js')
 
 const createSurvey = (data) => {
   return $.ajax({
-    url: config.apiOrigin + '/surveys',
+    url: config.apiOrigin + '/surveys/',
     method: 'POST',
     headers: {
       Authorization: 'Token token=' + store.user.token
@@ -32,20 +32,57 @@ const createQuestion = (data) => {
   })
 }
 
-const indexOfSurveys = function (data) {
+const indexOfSurveys = function () {
   return $.ajax({
     url: config.apiOrigin + '/surveys/',
+    method: 'GET'
+    // headers: {
+    //   Authorization: 'Token token=' + store.user.token
+    // }
+  })
+}
+
+const userIndex = function () {
+  return $.ajax({
+    url: config.apiOrigin + '/user-surveys',
     method: 'GET',
     headers: {
       Authorization: 'Token token=' + store.user.token
     },
-    data: data
+    data: {
+      'survey': {
+        'id': store.user._id
+      }
+    }
   })
 }
 
-const destroy = function (surveyId) {
+const surveyQuestions = function (id) {
+  console.log(id)
   return $.ajax({
-    url: config.apiOrigin + '/surveys/' + surveyId,
+    url: config.apiOrigin + '/questions',
+    method: 'GET',
+    data: {
+      'question': {
+        '_survey': id
+      }
+    }
+  })
+}
+
+const show = function (id) {
+  return $.ajax({
+    url: config.apiOrigin + '/surveys/' + id,
+    method: 'GET'
+    // headers: {
+    //   Authorization: 'Token token=' + store.user.token
+    // }
+  })
+}
+
+const destroy = function (id) {
+  return $.ajax({
+    url: config.apiOrigin + '/surveys/' + id,
     method: 'DELETE',
     headers: {
       Authorization: 'Token token=' + store.user.token
@@ -53,14 +90,28 @@ const destroy = function (surveyId) {
   })
 }
 
-const update = (surveyId, data) => {
+const update = (data, id) => {
   return $.ajax({
-    url: config.apiOrigin + '/surveys/' + surveyId,
+    url: config.apiOrigin + '/surveys/' + id,
     method: 'PATCH',
     headers: {
       Authorization: 'Token token=' + store.user.token
     },
-    data: data
+    data
+  })
+}
+
+const answerQuestion = (data, id) => {
+  return $.ajax({
+    url: config.apiOrigin + '/questions/' + id,
+    method: 'PATCH',
+    data: {
+      'question': {
+        'results': [{
+          'response': data
+        }]
+      }
+    }
   })
 }
 
@@ -68,6 +119,10 @@ module.exports = {
   createSurvey,
   createQuestion,
   update,
+  answerQuestion,
   indexOfSurveys,
+  userIndex,
+  surveyQuestions,
+  show,
   destroy
 }
---
+++ assets/scripts/surveys/apioriginal.js
@@ -0,0 +1,73 @@
+'use strict'
+
+const config = require('../config.js')
+const store = require('../store.js')
+
+const createSurvey = (data) => {
+  return $.ajax({
+    url: config.apiOrigin + '/surveys',
+    method: 'POST',
+    headers: {
+      Authorization: 'Token token=' + store.user.token
+    },
+    data
+  })
+}
+
+const createQuestion = (data) => {
+  console.log('store.surveyID: ' + store.surveyID)
+  console.log('data.prompt: ' + data.prompt)
+  return $.ajax({
+    url: config.apiOrigin + '/questions/',
+    method: 'POST',
+    headers: {
+      Authorization: 'Token token=' + store.user.token
+    },
+    data: {
+      'question': {
+        'prompt': data.prompt,
+        '_survey': store.surveyID
+      }
+    }
+  })
+}
+
+const indexOfSurveys = function (data) {
+  return $.ajax({
+    url: config.apiOrigin + '/surveys/',
+    method: 'GET',
+    headers: {
+      Authorization: 'Token token=' + store.user.token
+    },
+    data: data
+  })
+}
+
+const destroy = function (surveyId) {
+  return $.ajax({
+    url: config.apiOrigin + '/surveys/' + surveyId,
+    method: 'DELETE',
+    headers: {
+      Authorization: 'Token token=' + store.user.token
+    }
+  })
+}
+
+const update = (surveyId, data) => {
+  return $.ajax({
+    url: config.apiOrigin + '/surveys/' + surveyId,
+    method: 'PATCH',
+    headers: {
+      Authorization: 'Token token=' + store.user.token
+    },
+    data: data
+  })
+}
+
+module.exports = {
+  createSurvey,
+  createQuestion,
+  update,
+  indexOfSurveys,
+  destroy
+}
